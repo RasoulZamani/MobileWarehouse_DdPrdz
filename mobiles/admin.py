294aeb915c55e2d1b230fa960370042f31a21bbc
RasoulZamani <r.zamani72@gmail.com>
2023-08-05 18:34:26 +0330

    add model of mobiles in admin panel

--- mobiles/admin.py
+++ mobiles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Mobile
 # Register your models here.
+@admin.register(Mobile)
+class MobileAdmin(admin.ModelAdmin):
+    search_fields=('brand_name','nationality','model',
+                   'price','color','screen_size',
+                   'country_maker')
+    list_filter = ('brand_name','nationality','model',
+                   'price','color','screen_size',
+                   'country_maker','available')
