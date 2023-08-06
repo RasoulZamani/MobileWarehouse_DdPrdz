efb2e7df18f1039085aca047c9c6e7cbd90347db
RasoulZamani <r.zamani72@gmail.com>
2023-08-06 09:45:40 +0330

    adding search functionality to web

--- mobiles/admin.py
+++ mobiles/admin.py
@@ -3,9 +3,9 @@ from .models import Mobile
 # Register your models here.
 @admin.register(Mobile)
 class MobileAdmin(admin.ModelAdmin):
-    search_fields=('brand_name','nationality','model',
-                   'price','color','screen_size',
+    search_fields=('brand','nationality','model',
+                   'price','color',
                    'country_maker')
-    list_filter = ('brand_name','nationality','model',
+    list_filter = ('brand','nationality','model',
                    'price','color','screen_size',
                    'country_maker','available')
--- mobiles/views.py
+++ mobiles/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
+from .models import Mobile
+from django.db.models import Q
 
-# Create your views here.
+
 def search_in_mobiles(request):
     """searching in mobile warehouse"""
-    return render(request,'mobiles.html')
+    if request.method == "GET":
+        q_nationality = request.GET.get('nationality')
+        q_brand = request.GET.get('brand')
+
+        if q_brand == '':
+            q_brand = 'None'
+        if q_nationality == '':
+            q_nationality = 'None'
+
+        results = Mobile.objects.filter(
+            Q(nationality__icontains=q_nationality)  | Q(brand__icontains=q_brand) 
+            )
+
+    return render(request, 'mobiles.html',
+                  {'brand': q_brand,'nationality': q_nationality, 'results': results})
+
     
